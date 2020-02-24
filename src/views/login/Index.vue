455c0714d914495423696d11a582a6956fe94548
zyf <843403481@qq.com>
2020-02-24 16:30:07 +0800

    tab切换

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,16 +1,65 @@
 <template>
   <div id="login">
-    这里是登录页
+    <div class="login-wrap">
+      <ul class="menu-wrap">
+        <li
+          :class="{'current':item.current}"
+          v-for="item in menuTab"
+          :key="item.id"
+          @click="checkMenu(item)"
+        >{{item.txt}}</li>
+      </ul>
+    </div>
     <el-button>默认按钮</el-button>
   </div>
 </template>
 
 <script>
+export default {
+  name: "login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ]
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    checkMenu(data) {
+      this.menuTab.forEach(i => {
+        i.current = false;
+      });
+      data.current = true;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background-color: #344a5f;
+}
+.login-wrap {
+  width: 330px;
+  margin: 0 auto;
+}
+.menu-wrap {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 }
 </style>
